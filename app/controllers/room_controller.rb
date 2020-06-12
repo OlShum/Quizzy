8bacff343706fc31e9de029e4475a593618c3dff
OlShum <Olya@pp55.local>
2020-06-12 10:54:07 +0300

    Create question channel

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -10,5 +10,12 @@ class RoomController < ApplicationController
     # @questions.each do |question|
     #   @questions_array.push(question.as_json_for_rack)
     # end
+
+    # answered_questions_ids = @user.questions.collect { |q| q.id }
+    # questions = Question.all.reject { |q| answered_questions_ids.include?(q.id) }
+    # @question = questions.sample
+    # QuestionsUser.create!(user_id: @user.id, question_id: @question.id)
+    #
+    # render json: @question.as_json_for_rack
   end
 end
